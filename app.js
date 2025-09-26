7fd3a3301258015ae781aa99dcc1e7229c106303
borjoy27 <borjoy27@gmail.com>
2025-09-26 12:45:45 +0900

    week3 commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/*let startingPokerChips=100;
+let players=3;
+let noOfStaterCards=2;
+
+let playerOnePoints=startingPokerChips;
+let playerTwoPoints=startingPokerChips;
+let playerThreePoints=startingPokerChips;*/
+
+const STARTING_POKER_CHIPS=100;
+const PLAYERS=3;
+const NO_OF_STATER_CARDS=2;
+
+let playerOnePoints=STARTING_POKER_CHIPS;
+let playerTwoPoints=STARTING_POKER_CHIPS;
+let playerThreePoints=STARTING_POKER_CHIPS;
+
+playerOnePoints-=50;
+playerTwoPoints-=25;
+playerThreePoints+=75;
